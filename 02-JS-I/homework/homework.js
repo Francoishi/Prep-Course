2e6eef0e4c9baae72ed0b0856dbcf407858f0d68
unknown <franconu9@gmail.com>
2022-10-23 13:06:25 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,8 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = "3"
+
 
 // Crea una variable booleana:
 const nuevoBool = null;
